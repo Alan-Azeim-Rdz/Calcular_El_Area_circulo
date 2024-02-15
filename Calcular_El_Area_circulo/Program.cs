2e79810423580207261edaf60a93af498b8fd275
RODRIGUEZ VILLANUEVA ALAN AZEIM <I23050359@monclova.tecnm.mx>
2024-02-14 18:45:00 -0600

    Agregar archivos de proyecto.

---
+++ Calcular_El_Area_circulo/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Calculo_area_circulo
+{
+    static void Main(string[] args)
+    {
+        double radius, area;
+        string data_of_radius;
+
+        do
+        {
+            Console.WriteLine("Enter the radius of the circle:");
+            data_of_radius = Console.ReadLine();
+        }
+        while (!double.TryParse(data_of_radius, out radius));
+        radius = Convert.ToDouble(data_of_radius);
+
+        area= Math.PI * radius;
+
+        Console.WriteLine("The area of the circle is " + area);
+
+
+        Console.WriteLine("");
+
+    }
+}
